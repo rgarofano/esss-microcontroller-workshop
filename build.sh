949136ad160813fc33776e293a09794e5cd4fc32
Ryan Garofano <ryanjgarofano@gmail.com>
2024-02-21 21:23:09 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p build
+cd build
+cmake .. -DPICO_BOARD=pico_w
+make
---
+++ led.c
@@ -0,0 +1,12 @@
+#include <pico/stdlib.h>
+
+int main(void)
+{
+	// Initialize GPIO pin
+	
+	// Set GPIO pin direction
+	
+	while (true) {
+		// Make the LED blink
+	}
+}
